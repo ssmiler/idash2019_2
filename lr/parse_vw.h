9f493cf0f862ded60b8dcd610c9c641908350bb2
Sergiu Carpov <ssmiler@gmail.com>
2019-07-11 16:11:09 +0200

    comments

--- lr/parse_vw.h
+++ lr/parse_vw.h
@@ -2,4 +2,11 @@
 #include <string>
 #include <vector>
 
-void read(std::vector<std::unordered_map<std::string, float>>& coeffs, const std::string& file_name);
+/**
+ * @brief      Read a vowpal wabbit model file in human-readable form
+ *
+ * @param      coefs      The coefficients, where coefs[k] is a (feature name,
+ *                        value) map for predicting output==k (k=0..2)
+ * @param[in]  file_name  The file name
+ */
+void read(std::vector<std::unordered_map<std::string, float>>& coefs, const std::string& file_name);
