d3b4b9f649d7ba246fdb90269590e9fb8c496082
Sergiu Carpov <ssmiler@gmail.com>
2019-08-07 09:43:02 +0200

    added neighbors var

--- ml/learn_vw_2.sh
+++ ml/learn_vw_2.sh
@@ -3,13 +3,12 @@ jobs=4
 tag_type=10k
 # tag_type=1k
 
-
 data_out_path=data/vw/$tag_type/
 mkdir -p $data_out_path
 
-# target_snp=`cat data/target_snp`
-target_snp=`head -n 2 data/target_snp`
-target_snp+=" 16367555"
+target_snp=`cat data/target_snp`
+# target_snp=`head -n 2 data/target_snp`
+# target_snp+=" 16367555"
 
 function export_csv()
 {
@@ -18,13 +17,12 @@ function export_csv()
   val=${snp_val##*_}
   tag_type=$2
 
-  python3 stream_vw.py -n 20 --tag_file data/snp_tag_$tag_type.pickle -s $snp -v $val > data/vw/$tag_type/$snp_val.vw
+  neighbors=20
+
+  python3 stream_vw.py -n $neighbors --tag_file data/snp_tag_$tag_type.pickle -s $snp -v $val > data/vw/$tag_type/$snp_val.vw
 }
 export -f export_csv
 parallel -j $jobs export_csv {1/.}_{2} $tag_type ::: $target_snp ::: 0 1 2
-
-
-input_files=$data_out_path"/"*".vw"
 
 model_out_path=model/vw/$tag_type
 mkdir -p $model_out_path
@@ -37,9 +35,8 @@ function learn()
   # params="-k "
   params="-k --quiet "
   params+="--loss_function=logistic "
-  # params+="--oaa 3 "
   params+="--holdout_off "
-  params+="--passes 10 "
+  params+="--passes 20 "
   # params+="-q :: --leave_duplicate_interactions " #quadratic interactions
 
   cat $vw_data | head -n 2250 | vw $params -f $model --cache_file $model.cache
@@ -48,7 +45,7 @@ function learn()
 }
 export -f learn
 
-parallel -j $jobs learn $data_out_path/{1/.}.vw $model_out_path/{1/.}.model ::: $input_files
+parallel -j $jobs learn $data_out_path/{1}_{2}.vw $model_out_path/{1}_{2}.model ::: $target_snp ::: 0 1 2
 
 
 function test()
@@ -74,3 +71,13 @@ function test()
 export -f test
 parallel -j $jobs test {1} $data_out_path $model_out_path ::: $target_snp
 
+exit
+
+# model to human-readable format
+parallel -j $jobs vw --quiet -d {1} -i $model_out_path/{1}_{2}.model --invert_hash $model_out_path/{1}_{2}.model.hr -t ::: ::: $target_snp ::: 0 1 2
+
+
+#remove data and models
+
+rm $data_out_path -rf
+rm $model_out_path -rf
