f8395daf32c99386b5f5ff5b12e224c5bab2d086
Nicolas Gama <nicolas@inpher.io>
2019-08-12 15:06:36 -0700

    move some static const forward in the .h / .cpp

--- lr/idash.cpp
+++ lr/idash.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+
+//const uint32_t IdashParams::NUM_REGIONS = 2;
+//const uint32_t IdashParams::NUM_SNP_PER_POSITIONS = 3;
+
+//TRLWE parameters
+//const uint32_t IdashParams::N = 1024;              // TRLWE dimension
+//const uint32_t IdashParams::k = 1;                 // k is always 1
+const double IdashParams::alpha = pow(2., -20.);   // minimal noise   //TODO check the correct value for alpha
+//const uint32_t IdashParams::REGION_SIZE = IdashParams::N / IdashParams::NUM_REGIONS;           // N / NUM_REGIONS must be >= NUM_SAMPLES
+const TLweParams *IdashParams::tlweParams = new_TLweParams(IdashParams::N, IdashParams::k, IdashParams::alpha, 0.25);
+
+// SCALING_FACTOR is chosen at encryption
+// enc_data = one hot encoding of input / SCALING_FACTOR
+//            indexed by input feature name_snp: pos_0, pos_1, pos_2
+//            1 TRLWE packs the N samples
+const double IdashParams::IN_SCALING_FACTOR = 100;   // upon encryption, scale by IN_SCALING_FACTOR : double -> [-2^31,2^31[ //TODO check with Sergiu
+
+const Torus32 IdashParams::ONE_IN_T32 = Torus32(rint(IN_SCALING_FACTOR));
+const Torus32 IdashParams::NAN_0_IN_T32 = Torus32(
+        rint(3. / 6. * IN_SCALING_FACTOR));  // one hot encoding of NAN - value for snp 0
+const Torus32 IdashParams::NAN_1_IN_T32 = Torus32(
+        rint(2. / 6. * IN_SCALING_FACTOR));  // one hot encoding of NAN - value for snp 1
+const Torus32 IdashParams::NAN_2_IN_T32 = Torus32(
+        rint(1. / 6. * IN_SCALING_FACTOR));  // one hot encoding of NAN - value for snp 2
+
+
+
 /**
  * @brief      Split a string in format "pos_val" into a pair <pos,val>
  */
