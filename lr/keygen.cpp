fb9419ad8bba129f4c5cd30fd86cc6182799dd1c
Mariya <mariya@inpher.io>
2019-08-12 23:39:11 +0200

    keygen

--- lr/keygen.cpp
+++ lr/keygen.cpp
@@ -83,6 +83,7 @@ int main() {
     const std::string targetFile;
     const std::string challengeFile;
 
+
     IdashKey *key = keygen(targetFile, challengeFile);
     write_params(*key->idashParams, "params_filename");
     write_key(*key, "key_filename");
