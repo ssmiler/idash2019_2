2a10a6b72f67f81436bd02f4e14a27918ec80260
Mariya <mariya@inpher.io>
2019-08-15 16:25:41 +0200

    validation

--- lr/validation.cpp
+++ lr/validation.cpp
@@ -3,7 +3,7 @@
 int main() {
   Model model;
   PlaintextData data;
-  DecryptedPredictions targerPredictions;
+    DecryptedPredictions targetPredictions;
   EncryptedData encryptedData;
   DecryptedPredictions resultPredications;
   EncryptedPredictions encPredications;
@@ -13,11 +13,11 @@ int main() {
   read_model(model, params, "../../ml/model/hr/10k");
   read_plaintext_data(data, params, CHALLENGE_FILE);
 
-  compute_score(targerPredictions, data, model, params);
-  write_decrypted_predictions(targerPredictions, params, "target_preds_filename");
+    compute_score(targetPredictions, data, model, params);
+    write_decrypted_predictions(targetPredictions, params, "target_preds_filename");
 
   encrypt_data(encryptedData, data, *key);
   cloud_compute_score(encPredications, encryptedData, model, params);
   decrypt_predictions(resultPredications, encPredications, *key);
-  write_decrypted_predictions(targerPredictions, params, "res_preds_filename");
+    write_decrypted_predictions(targetPredictions, params, "res_preds_filename");
 }
