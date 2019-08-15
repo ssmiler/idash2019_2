ebf65098effb50031b81a8f64f2feb993022d606
Mariya <mariya@inpher.io>
2019-08-15 16:18:29 +0200

    validation

--- lr/decrypt.cpp
+++ lr/decrypt.cpp
@@ -12,66 +12,7 @@ using namespace std;
 
 //TODO shall we take in input enc_data or end_predictions?
 
-void
-decrypt_predictions(DecryptedPredictions &predictions, const EncryptedPredictions &enc_preds, const IdashKey &key) {
 
-    const IdashParams &params = *key.idashParams;
-    const uint64_t NUM_SAMPLES = params.NUM_SAMPLES;
-
-    TorusPolynomial *plain = new_TorusPolynomial(params.N);
-
-    for (const auto &it : params.out_features_index) {
-        const std::string &outPos = it.first;
-        for (int64_t snp = 0; snp < params.NUM_SNP_PER_POSITIONS; snp++) { // for snp in 0,1,2
-            FeatBigIndex outBIdx = it.second[snp];
-            const TLweSample *cipher = enc_preds.score.at(outBIdx);
-            tLwePhase(plain, cipher, key.tlweKey); // -> a rescaler
-            // initialize output
-            std::vector<float> &res = predictions.score[outPos][snp];  // this will create an empty vector in the result
-            res.resize(NUM_SAMPLES);
-            // rescale and copy result
-            for (uint64_t sample = 0; sample < NUM_SAMPLES; ++sample) {
-                res[sample] = plain->coefsT[sample];
-            }
-        }
-        // renormalize all probabilities
-        for (uint64_t sample = 0; sample < NUM_SAMPLES; ++sample) {
-            double x0 = max<double>(0, predictions.score[outPos][0][sample]);
-            double x1 = max<double>(0, predictions.score[outPos][1][sample]);
-            double x2 = max<double>(0, predictions.score[outPos][2][sample]);
-            double xNorm = x0 + x1 + x2;
-            if (xNorm > 0) {
-                // normalize so that the sum is +1
-                predictions.score[outPos][0][sample] = x0 / xNorm;
-                predictions.score[outPos][1][sample] = x1 / xNorm;
-                predictions.score[outPos][2][sample] = x2 / xNorm;
-            } else {
-                //consider these probas as NAN
-                predictions.score[outPos][0][sample] = 3. / 6.;
-                predictions.score[outPos][1][sample] = 2. / 6.;
-                predictions.score[outPos][2][sample] = 1. / 6.;
-            }
-        }
-    }
-
-/*
-    for (const auto &it : enc_preds.score) {
-
-        FeatBigIndex idx = it.first;
-        predictions.score.first = toString(idx);
-
-        TLweSample *cipher = it.second;
-
-        tLweSymDecrypt(plain, cipher, key.tlweKey, Msize);
-
-        //TODO how to retrieve the 3 probabilities?
-    }
-*/
-
-
-    // DELETE
-    delete_TorusPolynomial(plain);
-}
 
 int main() {
     IdashKey key;
--- lr/validation.cpp
+++ lr/validation.cpp
@@ -1,26 +1,23 @@
-#include <cstdio>
-#include "parse_vw.h"
+#include "idash.h"
 
-using namespace std;
+int main() {
+  Model model;
+  PlaintextData data;
+  DecryptedPredictions targerPredictions;
+  EncryptedData encryptedData;
+  DecryptedPredictions resultPredications;
+  EncryptedPredictions encPredications;
 
-int main(int argc, char** argv) {
-  if (argc < 2) {
-    printf("Usage: %s <*.model.hr>\n", argv[0]);
-    exit(0);
-  }
+  IdashKey *key = keygen(TARGET_FILE, CHALLENGE_FILE);
+  const IdashParams &params = *key->idashParams;
+  read_model(model, params, "../../ml/model/hr/10k");
+  read_plaintext_data(data, params, CHALLENGE_FILE);
 
-  vector<unordered_map<string, float>> coeffs;
-  read(coeffs, argv[1]);
+  compute_score(targerPredictions, data, model, params);
+  write_decrypted_predictions(targerPredictions, params, "target_preds_filename");
 
-  printf("Number of targets in model '%s': %ld\n", argv[1], coeffs.size());
-  for (int target = 0; target < coeffs.size(); ++target) {
-    printf("Target %d - %ld coefficients:\n", target, coeffs[target].size());
-    float coef_min = 1e20, coef_max = 1e-20;
-    for (auto& p: coeffs[target]) {
-      printf("\t%s %f\n", p.first.c_str(), p.second);
-      coef_min = min(coef_min, p.second);
-      coef_max = max(coef_max, p.second);
-    }
-    printf("\tmin-max: %f %f\n", coef_min, coef_max);
-  }
+  encrypt_data(encryptedData, data, *key);
+  cloud_compute_score(encPredications, encryptedData, model, params);
+  decrypt_predictions(resultPredications, encPredications, *key);
+  write_decrypted_predictions(targerPredictions, params, "res_preds_filename");
 }
