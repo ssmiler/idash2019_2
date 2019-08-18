58cbf427da990d7401d79490d0dc65ab3ece685a
Mariya <mariya@inpher.io>
2019-08-18 21:12:47 +0200

    split in .bin
    Former-commit-id: ba6a0223fe427ecf9f9769d652051d1d5e37a632

--- lr/decrypt.cpp
+++ lr/decrypt.cpp
@@ -10,18 +10,14 @@
 
 using namespace std;
 
-//TODO shall we take in input enc_data or end_predictions?
-
-
-
 int main() {
     IdashKey key;
     EncryptedPredictions enc_predictions;
     DecryptedPredictions dec_predictions;
 
-    read_key(key, "key_file");
-    read_encrypted_predictions(enc_predictions, *key.idashParams, "enc_prediction_file");
+    read_key(key, KEYS_FILE);
+    read_encrypted_predictions(enc_predictions, *key.idashParams, ENCRYPTED_PREDICTION_FILE);
     decrypt_predictions(dec_predictions, enc_predictions, key);
-    write_decrypted_predictions(dec_predictions, *key.idashParams, "dec_prediction_file", 0);
+    write_decrypted_predictions(dec_predictions, *key.idashParams, RESULT_FILE, 0);
+    write_decrypted_predictions(dec_predictions, *key.idashParams, RESULT_BYPOS_FILE, 1);
 }
-
--- lr/encrypt.cpp
+++ lr/encrypt.cpp
@@ -15,8 +15,8 @@ int main() {
     IdashKey key;
     PlaintextData plain_data;
     EncryptedData enc_data;
-    read_key(key, "key_file");
-    read_plaintext_data(plain_data, *key.idashParams, "plain_data_file");
+    read_key(key, KEYS_FILE);
+    read_plaintext_data(plain_data, *key.idashParams, CHALLENGE_FILE);
     encrypt_data(enc_data, plain_data, key);
-    write_encrypted_data(enc_data, *key.idashParams, "enc_data_file");
+    write_encrypted_data(enc_data, *key.idashParams, ENCRYPTED_DATA_FILE);
 }
