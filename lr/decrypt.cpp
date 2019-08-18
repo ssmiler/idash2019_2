f4acac2f71c92df242dfb69493fde197839a2fc0
Mariya <mariya@inpher.io>
2019-08-19 01:50:58 +0200

    test if the result is the same as validate2
    Former-commit-id: d07ad25391060aea7a31a3583cc3f64edf363478

--- lr/decrypt.cpp
+++ lr/decrypt.cpp
@@ -20,4 +20,15 @@ int main() {
     decrypt_predictions(dec_predictions, enc_predictions, key);
     write_decrypted_predictions(dec_predictions, *key.idashParams, RESULT_FILE, 0);
     write_decrypted_predictions(dec_predictions, *key.idashParams, RESULT_BYPOS_FILE, 1);
+
+    /** just to test if the result is the same as validate2
+    for (uint64_t sampleId = 0; sampleId < key.idashParams->NUM_SAMPLES; ++sampleId) {
+        for (const auto &it : key.idashParams->out_features_index) {
+            const uint64_t &pos = it.first;
+                std::cout << dec_predictions.score.at(pos)[0][sampleId] << std::endl;
+                std::cout << dec_predictions.score.at(pos)[1][sampleId] << std::endl;
+                std::cout << dec_predictions.score.at(pos)[2][sampleId] << std::endl;
+        };
+    }
+     */
 }
