4f8a7718f495ec12c21caf548f59933d540209ce
Nicolas Gama <nicolas@inpher.io>
2019-08-19 17:17:30 -0700

    add argv params
    Former-commit-id: 643a9c14e79ae305dd2e523c5c1959facd12aaa9

--- lr/encrypt.cpp
+++ lr/encrypt.cpp
@@ -11,12 +11,16 @@
 using namespace std;
 
 
-int main() {
+int main(int argc, char** argv) {
+    std::string challengeFile = CHALLENGE_FILE;
+    if (argc>=2) challengeFile = argv[1];
+    std::cout << "using tag file (challenge): " << challengeFile << std::endl;
+
     IdashKey key;
     PlaintextData plain_data;
     EncryptedData enc_data;
     read_key(key, KEYS_FILE);
-    read_plaintext_data(plain_data, *key.idashParams, CHALLENGE_FILE);
+    read_plaintext_data(plain_data, *key.idashParams, challengeFile);
     encrypt_data(enc_data, plain_data, key);
     write_encrypted_data(enc_data, *key.idashParams, ENCRYPTED_DATA_FILE);
 }
