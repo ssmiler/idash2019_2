9feacd23846016818771295007a359fccdba9daa
Sergiu Carpov <sergiu@inpher.io>
2020-04-27 10:52:01 +0200

    run tests with different core count

--- lr/run_test.sh
+++ lr/run_test.sh
@@ -20,32 +20,43 @@ df.loc[:, lb].to_csv('$outFile', index=False, header=False, sep='\t')
 export -f get_dataset
 
 idxFileA=../../data/testing_sample_ids.list
-for nb_targets in 80882 20000 40000
-do
-    head -n $nb_targets ../../data/target_geno_model_coordinates.txt > targets_tmp.txt
-    for suffix in "" "_AFR" "_AMR" "_EUR"
+
+for nb_cores in 1 4 8
     do
-        idxFileB=../../data/testing_sample_ids$suffix.list
 
-        get_dataset $idxFileA $idxFileB ../../data/tag_testing.txt tag_testing$suffix.txt
-        get_dataset $idxFileA $idxFileB ../../data/target_testing.txt target_testing$suffix.txt
+    sed -i "s/#define NB_THREADS.*/#define NB_THREADS $nb_cores/" ../idash.h
+    make build
 
-        for n in 5 10 15 20 25 30 35 40 45 50
+    for nb_targets in 80882 20000 40000
+    do
+        head -n $nb_targets ../../data/target_geno_model_coordinates.txt > targets_tmp.txt
+        for suffix in "" "_AFR" "_AMR" "_EUR"
         do
-            sed -i "s/MODEL_FILE=.*/MODEL_FILE=\"..\/..\/ml\/model\/hr\/neighbors="$n"_scale=16384$suffix\"/" Makefile-final.inc
-            sed -i "s/TARGET_HEADERS=.*/TARGET_HEADERS=targets_tmp.txt/" Makefile-final.inc
-            sed -i "s/CHALLENGE_FILE=.*/CHALLENGE_FILE=tag_testing$suffix.txt/" Makefile-final.inc
-            sed -i "s/TARGET_FILE=.*/TARGET_FILE=target_testing$suffix.txt/" Makefile-final.inc
+            idxFileB=../../data/testing_sample_ids$suffix.list
 
-            make clean > /dev/null
-            log_file=logs/$n$suffix"_"$nb_targets.log
-            make result.csv > $log_file
+            get_dataset $idxFileA $idxFileB ../../data/tag_testing.txt tag_testing$suffix.txt
+            get_dataset $idxFileA $idxFileB ../../data/target_testing.txt target_testing$suffix.txt
 
-            python3 ../parse_log.py $log_file $nb_targets,$suffix,$n
+            for n in 5 10 15 20 25 30 35 40 45 50
+            do
+                sed -i "s/MODEL_FILE=.*/MODEL_FILE=\"..\/..\/ml\/model\/hr\/neighbors="$n"_scale=16384$suffix\"/" Makefile-final.inc
+                sed -i "s/TARGET_HEADERS=.*/TARGET_HEADERS=targets_tmp.txt/" Makefile-final.inc
+                sed -i "s/CHALLENGE_FILE=.*/CHALLENGE_FILE=tag_testing$suffix.txt/" Makefile-final.inc
+                sed -i "s/TARGET_FILE=.*/TARGET_FILE=target_testing$suffix.txt/" Makefile-final.inc
 
-	    # make auc.png
+                make clean > /dev/null
+                log_file=logs/$n$suffix"_"$nb_targets.log
+                make result.csv > $log_file
+
+                python3 ../parse_log.py $log_file $nb_cores,$nb_targets,$suffix,$n
+
+                # make auc.png
+            done
+
+            rm tag_testing$suffix.txt
+            rm target_testing$suffix.txt
         done
+
+        rm targets_tmp.txt
     done
 done
-
-rm targets_tmp.txt
