776da6a517a0d0865de8a335d6ee19eba76965b7
Sergiu Carpov <ssmiler@gmail.com>
2021-02-26 20:04:46 +0100

    Update prepare_data.py

--- train/prepare_data.py
+++ train/prepare_data.py
@@ -23,19 +23,22 @@ def read_and_transform(inp_file):
     return df
 
 
-def read_ids(inp_file):
-    return np.array(list(map(lambda l: l.strip(), open(inp_file).readlines())))
+def read_ids(inp_file, assert_file_exists = True):
+    if os.path.exists(inp_file):
+        return np.array(list(map(lambda l: l.strip(), open(inp_file).readlines())))
+    assert(not assert_file_exists), "File {} not found.".format(inp_file)
+    return None
 
 
 all_ids_train = read_ids(orig_data_path + '/training_sample_ids.list')
-pop_ids_AFR_train = read_ids(orig_data_path + '/training_sample_ids_AFR.list')
-pop_ids_AMR_train = read_ids(orig_data_path + '/training_sample_ids_AMR.list')
-pop_ids_EUR_train = read_ids(orig_data_path + '/training_sample_ids_EUR.list')
+pop_ids_AFR_train = read_ids(orig_data_path + '/training_sample_ids_AFR.list', False)
+pop_ids_AMR_train = read_ids(orig_data_path + '/training_sample_ids_AMR.list', False)
+pop_ids_EUR_train = read_ids(orig_data_path + '/training_sample_ids_EUR.list', False)
 
 all_ids_test = read_ids(orig_data_path + '/testing_sample_ids.list')
-pop_ids_AFR_test = read_ids(orig_data_path + '/testing_sample_ids_AFR.list')
-pop_ids_AMR_test = read_ids(orig_data_path + '/testing_sample_ids_AMR.list')
-pop_ids_EUR_test = read_ids(orig_data_path + '/testing_sample_ids_EUR.list')
+pop_ids_AFR_test = read_ids(orig_data_path + '/testing_sample_ids_AFR.list', False)
+pop_ids_AMR_test = read_ids(orig_data_path + '/testing_sample_ids_AMR.list', False)
+pop_ids_EUR_test = read_ids(orig_data_path + '/testing_sample_ids_EUR.list', False)
 
 df = read_and_transform(orig_data_path + '/tag_training.txt')
 df.to_pickle(out_data_path + '/tag_train.pickle')
@@ -43,12 +46,15 @@ print('Tag SNPs training data shape: ', df.shape)
 
 # export population stratified datasets
 assert(len(all_ids_train) == len(df))
-df.iloc[np.isin(all_ids_train, pop_ids_AFR_train)].to_pickle(
-    out_data_path + '/tag_train_AFR.pickle')
-df.iloc[np.isin(all_ids_train, pop_ids_AMR_train)].to_pickle(
-    out_data_path + '/tag_train_AMR.pickle')
-df.iloc[np.isin(all_ids_train, pop_ids_EUR_train)].to_pickle(
-    out_data_path + '/tag_train_EUR.pickle')
+if pop_ids_AFR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_AFR_train)].to_pickle(
+        out_data_path + '/tag_train_AFR.pickle')
+if pop_ids_AMR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_AMR_train)].to_pickle(
+        out_data_path + '/tag_train_AMR.pickle')
+if pop_ids_EUR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_EUR_train)].to_pickle(
+        out_data_path + '/tag_train_EUR.pickle')
 
 df = read_and_transform(orig_data_path + '/tag_testing.txt')
 df.to_pickle(out_data_path + '/tag_test.pickle')
@@ -56,12 +62,15 @@ print('Tag SNPs testing data shape: ', df.shape)
 
 # export population stratified datasets
 assert(len(all_ids_test) == len(df))
-df.iloc[np.isin(all_ids_test, pop_ids_AFR_test)].to_pickle(
-    out_data_path + '/tag_test_AFR.pickle')
-df.iloc[np.isin(all_ids_test, pop_ids_AMR_test)].to_pickle(
-    out_data_path + '/tag_test_AMR.pickle')
-df.iloc[np.isin(all_ids_test, pop_ids_EUR_test)].to_pickle(
-    out_data_path + '/tag_test_EUR.pickle')
+if pop_ids_AFR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_AFR_test)].to_pickle(
+        out_data_path + '/tag_test_AFR.pickle')
+if pop_ids_AMR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_AMR_test)].to_pickle(
+        out_data_path + '/tag_test_AMR.pickle')
+if pop_ids_EUR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_EUR_test)].to_pickle(
+        out_data_path + '/tag_test_EUR.pickle')
 
 df = read_and_transform(orig_data_path + '/target_training.txt')
 df.to_pickle(out_data_path + '/target_train.pickle')
@@ -69,12 +78,15 @@ print('Target SNPs training data shape: ', df.shape)
 
 # export population stratified datasets
 assert(len(all_ids_train) == len(df))
-df.iloc[np.isin(all_ids_train, pop_ids_AFR_train)].to_pickle(
-    out_data_path + '/target_train_AFR.pickle')
-df.iloc[np.isin(all_ids_train, pop_ids_AMR_train)].to_pickle(
-    out_data_path + '/target_train_AMR.pickle')
-df.iloc[np.isin(all_ids_train, pop_ids_EUR_train)].to_pickle(
-    out_data_path + '/target_train_EUR.pickle')
+if pop_ids_AFR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_AFR_train)].to_pickle(
+        out_data_path + '/target_train_AFR.pickle')
+if pop_ids_AMR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_AMR_train)].to_pickle(
+        out_data_path + '/target_train_AMR.pickle')
+if pop_ids_EUR_train:
+    df.iloc[np.isin(all_ids_train, pop_ids_EUR_train)].to_pickle(
+        out_data_path + '/target_train_EUR.pickle')
 
 df = read_and_transform(orig_data_path + '/target_testing.txt')
 df.to_pickle(out_data_path + '/target_test.pickle')
@@ -82,11 +94,14 @@ print('Target SNPs testing data shape: ', df.shape)
 
 # export population stratified datasets
 assert(len(all_ids_test) == len(df))
-df.iloc[np.isin(all_ids_test, pop_ids_AFR_test)].to_pickle(
-    out_data_path + '/target_test_AFR.pickle')
-df.iloc[np.isin(all_ids_test, pop_ids_AMR_test)].to_pickle(
-    out_data_path + '/target_test_AMR.pickle')
-df.iloc[np.isin(all_ids_test, pop_ids_EUR_test)].to_pickle(
-    out_data_path + '/target_test_EUR.pickle')
+if pop_ids_AFR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_AFR_test)].to_pickle(
+        out_data_path + '/target_test_AFR.pickle')
+if pop_ids_AMR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_AMR_test)].to_pickle(
+        out_data_path + '/target_test_AMR.pickle')
+if pop_ids_EUR_test:
+    df.iloc[np.isin(all_ids_test, pop_ids_EUR_test)].to_pickle(
+        out_data_path + '/target_test_EUR.pickle')
 
 open(out_data_path + '/target_snp', 'w').write('\n'.join(map(str, df.columns)))
